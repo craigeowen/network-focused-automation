a9e63b79d9d8f27292f0d0c103106000a8fe3fcc
Craig <craigeowen@gmail.com>
2021-05-19 15:23:56 +0100

    V2

--- IOS_Pyton_Lab/isco-automation-tutorial.py
+++ IOS_Pyton_Lab/isco-automation-tutorial.py
@@ -4,3 +4,22 @@
 # Import the required dependencies
 from ncclient import manager
 from jinja2 import Template
+# Establish our NETCONF Session
+m = manager.connect(host='192.168.128.37', port=830, username='admin',
+                    password='admin', device_params={'name': 'csr'})
+# Create a configuration filter
+interface_filter = '''
+  <filter>
+      <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+          <interface>
+            <GigabitEthernet>
+              <name>0/1</name>
+            </GigabitEthernet>
+          </interface>
+      </native>
+  </filter>
+'''
+
+# Execute the get-config RPC
+result = m.get_config('running', interface_filter)
+print(result)
