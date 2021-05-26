a123dfdbe229a87640dd4df1f8fe7451b0aba848
Craig <craigeowen@gmail.com>
2021-05-26 08:07:42 +0100

    IOS Lab v1

--- IOS_Pyton_Lab/ios-automation-tutorial-nxos-test.py
+++ IOS_Pyton_Lab/ios-automation-tutorial-nxos-test.py
@@ -6,20 +6,9 @@ from ncclient import manager
 from jinja2 import Template
 # Establish our NETCONF Session
 m = manager.connect(host='192.168.128.37', port=830, username='admin',
-                    password='admin', device_params={'name': 'csr'})
+                    password='admin', device_params={'name': 'nexus'})
 # Create a configuration filter
-interface_filter = '''
-  <filter>
-      <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-          <interface>
-            <GigabitEthernet>
-              <name>0/1</name>
-            </GigabitEthernet>
-          </interface>
-      </native>
-  </filter>
-'''
 
 # Execute the get-config RPC
-result = m.get_config('running', interface_filter)
+result = m.get_config('running')
 print(result)
